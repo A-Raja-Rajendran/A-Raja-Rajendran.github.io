928ac1a30c8396d7f7b99559bf3d469f8ffc349e
A. Raja Rajendran <70101699+A-Raja-Rajendran@users.noreply.github.com>
2021-02-28 14:38:22 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,231 @@
+var chosenOne = symbols(randomnum(1, 20));
+document.getElementById("num01").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num02").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num03").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num04").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num05").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num06").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num07").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num08").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num09").innerHTML = chosenOne;
+document.getElementById("num10").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num11").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num12").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num13").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num14").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num15").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num16").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num17").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num18").innerHTML = chosenOne;
+document.getElementById("num19").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num20").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num21").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num22").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num23").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num24").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num25").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num26").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num27").innerHTML = chosenOne;
+document.getElementById("num28").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num29").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num30").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num31").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num32").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num33").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num34").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num35").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num36").innerHTML = chosenOne;
+document.getElementById("num37").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num38").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num39").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num40").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num41").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num42").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num43").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num44").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num45").innerHTML = chosenOne;
+document.getElementById("num46").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num47").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num48").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num49").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num50").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num51").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num52").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num53").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num54").innerHTML = chosenOne;
+document.getElementById("num55").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num56").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num57").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num58").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num59").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num60").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num61").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num62").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num63").innerHTML = chosenOne;
+document.getElementById("num64").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num65").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num66").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num67").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num68").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num69").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num70").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num71").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num72").innerHTML = chosenOne;
+document.getElementById("num73").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num74").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num75").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num76").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num77").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num78").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num79").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num80").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num81").innerHTML = chosenOne;
+document.getElementById("num82").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num83").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num84").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num85").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num86").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num87").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num88").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num89").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num90").innerHTML = chosenOne;
+document.getElementById("num91").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num92").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num93").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num94").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num95").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num96").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num97").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num98").innerHTML = symbols(randomnum(1, 20) );
+document.getElementById("num99").innerHTML = symbols(randomnum(1, 20) );
+ document.getElementById("mod").innerHTML = chosenOne;
+             
+// for the symbols
+function symbols(x)
+{
+    var dum1 = " ! *-* ! ";
+    var dum2 = "( *^-^* )";
+    var dum3 = " (@ _ @ )";
+    var dum4 = " I* - *I";
+    var dum5 = " (`-`)";
+    var dum6 = " (-__-)";
+    var dum7 = " [() ()]";
+    var dum8 = " { ^v^ }";
+    var dum9 = " < @-@ >";
+    var dum10 = " (T_T)";
+    var dum11 = " ||o-o||";
+    var dum12 = " [b _ d]";
+    var dum13 = "{- v -}";
+    var dum14 = " | +_+ |";
+    var dum15 = " (~ v ~)";
+    var dum16 = "=(o - o)= ";
+    var dum17 = "--(**)--";
+    var dum18 = " |U U| ";
+    var dum19 = " /$ - $/ ";
+    var dum20 = " (/ UwU)/";
+
+    if(x==1)
+    {
+        return dum1;
+    }
+    else if(x==2)
+    {
+        return dum2;
+    }
+    else if(x==3)
+    {
+        return dum3;
+    }
+    else if(x==4)
+    {
+        return dum4;
+    }
+    else if(x==5)
+    {
+        return dum5;
+    }
+    else if(x==6)
+    {
+        return dum6;
+    }
+    else if(x==7)
+    {
+        return dum7;
+    }
+    else if(x==8)
+    {
+        return dum8;
+    }
+    else if(x==9)
+    {
+        return dum9;
+    }
+    else if(x==10)
+    {
+        return dum10;
+    }
+    else if(x==11)
+    {
+        return dum11;
+    }
+    else if(x==12)
+    {
+        return dum12;
+    }
+    else if(x==13)
+    {
+        return dum13;
+    }
+    else if(x==14)
+    {
+        return dum14;
+    }
+    else if(x==15)
+    {
+        return dum15;
+    }
+    else if(x==16)
+    {
+        return dum16;
+    }
+    else if(x==17)
+    {
+        return dum17;
+    }
+    else if(x==18)
+    {
+        return dum18;
+    }
+    else if(x==19)
+    {
+        return dum19;
+    }
+    else if(x==20)
+    {
+        return dum20;
+    }
+}
+
+
+//for generating random number
+function randomnum(min, max) 
+{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() 
+{
+     modal.style.display = "block";
+}
